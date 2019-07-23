99e75b3a9beec5d3f5c5f87dcb7e37260cd0d0d2
Manjot Singh <manjot.kalsi@simbaquartz.com>
2019-07-23 11:25:03 +0530

    Tabbar reverted. Society added

--- src/components/navigation/TabBar/TabBar.js
+++ src/components/navigation/TabBar/TabBar.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Image, View, FlatList } from "react-native";
 
 import {
@@ -11,7 +11,76 @@ import {
   Icon,
   Footer
 } from "native-base";
-import { createBottomTabNavigator, BottomTabBar } from "react-navigation-tabs";
-export default (TabBarComponent = props => {
-  return <BottomTabBar {...props} />;
-});
+
+const main_routes = [
+  {
+    title: "Societies",
+    icon: { name: "globe", type: "Ionicons" }
+  },
+  {
+    title: "News",
+    icon: { name: "notifications", type: "Ionicons" }
+  },
+  {
+    title: "Home",
+    icon: { name: "home", type: "Ionicons" }
+  },
+  {
+    title: "Quora",
+    icon: { name: "planet", type: "Ionicons" }
+  },
+
+  {
+    title: "Profile",
+    icon: { name: "person", type: "Ionicons" }
+  }
+];
+
+export default class TabBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    console.log("Tab Bar did mount");
+  }
+
+  render() {
+    return (
+      <Container
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          flex: 0.067,
+          backgroundColor: "#E91E63",
+          paddingBottom: 10
+        }}
+      >
+        <View style={{ alignItems: "center" }}>{this.Tab()}</View>
+      </Container>
+    );
+  }
+
+  Tab() {
+    return (
+      <FlatList
+        keyExtractor={(item, index) => index.toString()}
+        data={main_routes}
+        numColumns={5}
+        renderItem={({ item }) => (
+          <View>
+            <ListItem
+              onPress={() => this.props.navigation.navigate(item.title)}
+            >
+              <Icon
+                name={item.icon.name}
+                type={item.icon.type}
+                style={{ color: "white", fontSize: 26, marginRight: 18 }}
+              />
+            </ListItem>
+          </View>
+        )}
+      />
+    );
+  }
+}
